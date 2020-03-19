396686e408d9afa3c5cbb528478afcbe41e29af4
eugene-finn <1287175@gmail.com>
2020-03-19 14:07:20 +0200

    create div and some pip

---
+++ src/components/App/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <p className="description">Миру-мир, студентам - beer.</p>
+      </div>
+    );
+  }
+}
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+export default App;
